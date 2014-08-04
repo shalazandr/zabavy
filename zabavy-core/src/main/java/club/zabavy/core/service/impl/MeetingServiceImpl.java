724df647a04179d45aa13cb87c5fddb6141e05af
Andrew Shalankevych <shalankevych@gmail.com>
2014-08-04 22:50:19 +0300

    fill null fields for Meeting in MeetingService

--- zabavy-core/src/main/java/club/zabavy/core/service/impl/MeetingServiceImpl.java
+++ zabavy-core/src/main/java/club/zabavy/core/service/impl/MeetingServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -29,6 +30,12 @@ public class MeetingServiceImpl implements MeetingService {
 	@Override
 	public void insert(Meeting meeting) {
 		if(meeting.getInitiator() == null) throw new NullPointerException("Initiator cant be null.");
+		if(meeting.getStatus() == null) meeting.setStatus(Meeting.Status.PLANNED);
+		if(meeting.getType() == null) meeting.setType(Meeting.Type.PRIVATE);
+		if(meeting.getTitle() == null) meeting.setTitle("");
+		if(meeting.getDate() == null) meeting.setDate(new Date());
+		if(meeting.getPlace() == null) meeting.setPlace("");
+
 		meetingDAO.insert(meeting);
 	}
 
