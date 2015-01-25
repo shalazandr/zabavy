30e97b5ff7e404fcd81ba49fd11d495d153cb3ae
Andrew Shalankevych <shalankevych@gmail.com>
2015-01-25 05:53:32 +0200

    add exception handler

---
+++ zabavy-core/src/main/java/club/zabavy/core/controller/ExceptionHandlingController.java
@@ -0,0 +1,34 @@
+package club.zabavy.core.controller;
+
+import club.zabavy.core.domain.exceptions.NotAuthenticatedUserException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class ExceptionHandlingController {
+
+	@ExceptionHandler(NotAuthenticatedUserException.class)
+	@ResponseBody
+	@ResponseStatus(HttpStatus.UNAUTHORIZED)
+	public Map handleError(NotAuthenticatedUserException exception) {
+		HashMap error = new HashMap();
+		error.put("message", exception.getMessage());
+		return error;
+	}
+
+	@ExceptionHandler(Exception.class)
+	@ResponseBody
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public Map handleError(HttpServletRequest req, Exception exception) {
+		HashMap error = new HashMap();
+		error.put("message", exception.getMessage());
+		return error;
+	}
+}
