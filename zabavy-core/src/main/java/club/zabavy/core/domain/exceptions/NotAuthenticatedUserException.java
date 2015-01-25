d4f44873e7243474b6c91a14d1391dc4eb07d4d0
Andrew Shalankevych <shalankevych@gmail.com>
2015-01-25 05:54:32 +0200

    ...

--- zabavy-core/src/main/java/club/zabavy/core/domain/exceptions/NotAuthenticatedUserException.java
+++ zabavy-core/src/main/java/club/zabavy/core/domain/exceptions/NotAuthenticatedUserException.java
@@ -2,4 +2,9 @@ package club.zabavy.core.domain.exceptions;
 
 public class NotAuthenticatedUserException extends RuntimeException {
 
+	public NotAuthenticatedUserException() {}
+
+	public NotAuthenticatedUserException(String message) {
+		super(message);
+	}
 }
