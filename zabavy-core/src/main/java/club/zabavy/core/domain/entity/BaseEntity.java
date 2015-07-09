7235caff66a56defd060b81884e27f2670ebf767
Andrew Shalankevych <shalankevych@gmail.com>
2015-07-09 13:42:00 +0300

    create super class for entities

---
+++ zabavy-core/src/main/java/club/zabavy/core/domain/entity/BaseEntity.java
@@ -0,0 +1,43 @@
+package club.zabavy.core.domain.entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.Date;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+	@Id
+	@GeneratedValue
+	protected Long id;
+	@Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+	protected Date createdAt;
+	@Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+	protected Date updatedAt;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+}
